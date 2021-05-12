65f8375c37499933e29ed436b757d2718dfcb560
av.morozov <bishmoroz@gmail.com>
2021-05-12 17:29:15 +0400

    Homework. Lesson1

---
+++ Homework/src/main/java/ru/vtb/java/lesson1/vehicles/bike.java
@@ -0,0 +1,7 @@
+package ru.vtb.java.lesson1.vehicles;
+
+public class bike extends baseVehicle {
+    public bike(String name){
+        super(name, "bike");
+    }
+}
---
+++ Homework/src/main/java/ru/vtb/java/lesson1/vehicles/car.java
@@ -0,0 +1,7 @@
+package ru.vtb.java.lesson1.vehicles;
+
+public class car extends baseVehicle{
+    public car(String name){
+        super(name, "car");
+    }
+}
